f774416920b37dab995ed5d07bfb4802edbee1b6
João Vítor Matos dos Santos <jvmatossantos00@gmail.com>
2026-05-13 15:17:07 -0300

    Adicionando handleclick no botao

--- Product-cart/src/components/Buttons/Button.tsx
+++ Product-cart/src/components/Buttons/Button.tsx
@@ -1,13 +1,18 @@
 import iconAddToCart from '../../../public/images/icon-add-to-cart.svg';
 import '../../stylesComponents/ButtonStyles.css';
+import { useState } from 'react';
 
 function Button() {
+  const [isActive, setIsActive] = useState(false);
 
   /*criar função para adicionar ao carrinho*/
+  const handleClick = () =>{
+    setIsActive(!isActive);
+  }
 
   return (
     <>
-        <button className='add-button'> <img src={iconAddToCart} alt="Add to cart" />Add to Cart</button>
+        <button className={`add-button ${isActive ? "active" : ""}`} onClick={handleClick}> <img src={iconAddToCart} alt="Add to cart" />Add to Cart</button>
     </>
   )
 }
