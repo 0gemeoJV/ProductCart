9e151c313f1f9123b422303315bf8bdf6b4deb73
Pedro Enrique <penriquematos@gmail.com>
2026-05-20 17:51:17 -0300

    estilizando YourCart

--- Product-cart/src/components/ItemCard.tsx
+++ Product-cart/src/components/ItemCard.tsx
@@ -5,10 +5,10 @@ function ItemCard({ item, index }: { item: any; index: number }) {
     return (
         <div className='item-card' key={index}>
             <div className='item-details'>
-                <h3>{item.name}</h3>
+                <h3 className='item-name'>{item.name}</h3>
                 <div className='item-pricing'>
                     <span className='item-qty'>{item.quantity}x</span>
-                    <span className='item-price'>${item.price.toFixed(2)}</span>
+                    <span className='item-price'>@ ${item.price.toFixed(2)}</span>
                     <span className='item-total-price'>${(item.quantity * item.price).toFixed(2)}</span>
                 </div>
             </div>
